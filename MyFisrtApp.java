dc8ab494316bc5c8131f7bb22b289627b3a9ee8e
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-04-12 22:02:10 +0530

    Build simple gui using Java Awt
    Tool:-Apache NetBeans

---
+++ MyFisrtApp.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+public class MyFisrtApp {
+    public static void main(String[] args) {
+        Frame fr=new Frame("My First App");
+        Button b=new Button("ok");
+        fr.add(b);
+        fr.setSize(300,300);
+        fr.setVisible(true);
+        
+
+    }
+    
+}
+ 
